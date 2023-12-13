514bdd6f213cafcefc64a8ea2a6e65476a1115cb
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-12-13 07:28:29 +0100

    feat: Nvabar completion

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,47 @@
-import {headerLogo} from '../assets/images'
-import { hamburger} from "../assets/icons"
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
 const Nav = () => {
   return (
-   <header>
-    <nav>
-      <a href="/">
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img 
+            src={headerLogo} 
+            alt="header-logo"
+            width={130}
+            height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item)=>(
+            <li key={item.label}>
+              <a
+               href={item.href}
+               className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
 
-        <img src={headerLogo} alt="header-logo"/>
+              </a>
 
-      </a>
-    </nav>
-   </header>
-  )
-}
+            </li>
 
-export default Nav
+          ))}
+
+        </ul>
+        <div className="hidden max-lg:block">
+          <img 
+            src={hamburger} 
+            alt="Hamburger"
+            width={25}
+          />
+
+
+        </div>
+
+        
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
