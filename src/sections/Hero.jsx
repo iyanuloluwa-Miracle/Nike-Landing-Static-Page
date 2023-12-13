a38d0725be3d02bfe13ccfcc90f286f105902c10
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-12-13 22:41:57 +0100

    feat: Added hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,38 @@
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+import { statistics } from "../constants";
 const Hero = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container "
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
+          <br />
+          <span className="">Nike</span>Shoes
+         
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+        <Button 
+          label="Shop now"
+          iconURL={arrowRight}/>
+      </div>
 
-export default Hero
+      <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+        {statistics.map((stat) =>(
+          <div key={stat.label}>
+            <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+            <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
+          </div>
+
+        ))}
+
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
