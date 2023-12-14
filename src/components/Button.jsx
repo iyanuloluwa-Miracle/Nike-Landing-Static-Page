27143eec7261a4328fb8fd1911056569ff20825d
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-12-14 15:00:07 +0100

    fix: Bug fix in the hero section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 
 const Button = ({ label, iconURL}) => {
   return (
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,8 @@
 import Button from "../components/Button";
 import { arrowRight } from "../assets/icons";
-import { statistics } from "../constants";
+import { shoes, statistics } from "../constants";
+import { bigShoe1 } from "../assets/images";
+import ShoeCard from "../components/ShoeCard";
 const Hero = () => {
   return (
     <section
@@ -8,27 +10,56 @@ const Hero = () => {
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container "
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collection
+        </p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
           <br />
-          <span className="">Nike</span>Shoes
-         
+          <span className="">Nike</span> Shoes
         </h1>
-        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
-        <Button 
-          label="Shop now"
-          iconURL={arrowRight}/>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+        <Button label="Shop now" iconURL={arrowRight} />
+
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
 
-      <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
-        {statistics.map((stat) =>(
-          <div key={stat.label}>
-            <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
-            <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
-          </div>
 
-        ))}
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img 
+          src={bigShoe1}
+          alt="Shoe Collection"
+          width={610}
+          height={500}
+          className="object-contain relative z-10" 
+        />
+
+        <div>
+          {shoes.map((shoe)=>(
+            <div key={shoe}>
+              <ShoeCard
+              imgURL={shoe}
+              changeBigShoeImage={() =>{}}
+              bigShoeImg=""
+              />
+
+            </div>
+          ))}
+        </div>
 
       </div>
     </section>
