45c5ca232359127a10ca9711f8fdf5a603d10072
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-12-21 08:14:11 +0100

    feat:SuperQuality Section added

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,9 +1,30 @@
-
+import Button from "../components/Button";
 const SuperQuality = () => {
   return (
-    <div>
-      
-    </div>
+    <section
+    id="about-us"
+    className="flex justify-between items-center max-lg:flex-col items-center max-lg:flex-col gap-10 w-full max-container">
+      <div className="flex flex-1 flex-col">
+        <h2 className='mt-10 font-palanquin text-4xl capitalize font-bold lg:max-w-lg'>
+          We Provide You
+          <span className="text-coral-red">
+             Super
+          </span>
+          <span className="text-coral-red">
+          Quality
+          </span> Shoes          
+          <br />
+        </h2>
+        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+
+        <Button label='View details'/>
+
+      </div>
+
+    </section>
   )
 }
 
