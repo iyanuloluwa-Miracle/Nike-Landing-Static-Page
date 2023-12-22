a47e328e190735a3a1a57e37579203aa69d93908
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-12-22 21:17:08 +0100

    feat: Subscribe section completed

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,10 +1,32 @@
+import ReviewCard  from "../components/ReviewCard"
+import { reviews } from "../constants/index";
+
 const CustomerReviews = () => {
   return (
-    <div>
-        <h1></h1>
-      
-    </div>
-  )
-}
+    <section className='max-container'>
+      <h3 className='font-palanquin text-center text-4xl font-bold'>
+        What Our
+        <span className='text-coral-red'> Customers </span>
+        Say?
+      </h3>
+      <p className='m-auto mt-4 max-w-lg  text-center info-text'>
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
 
-export default CustomerReviews
+      <div className='mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14'>
+        {reviews.map((review, index) => (
+          <ReviewCard
+            key={index}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default CustomerReviews;
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,9 +1,13 @@
 
 const Subscribe = () => {
   return (
-    <div>
+    <section className="max-container flex justify-between items-center max-lg:flex-col gap-10 " id="contact-us">
+      <h3 className="text-4xl leading-[68px] lg:max-w-md">Sign Up form</h3>
+      <span>
+        Updates
+      </span> & Newsletter
       
-    </div>
+    </section>
   )
 }
 
